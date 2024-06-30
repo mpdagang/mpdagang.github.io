61698f03f35fbc72a7e74d05c748572046fb91db
mpdagang95 <83046305+mpdagang95@users.noreply.github.com>
2024-06-30 17:10:27 +0800

    Add null check to current js functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,25 +12,31 @@ document.addEventListener("DOMContentLoaded", function() {
     }
     
     let hPanelExit = document.getElementById("h-panel-exit");
-    hPanelExit.addEventListener("click", function(){
-        let panel = document.getElementById("test");
-        panel.classList.toggle("move");
-        let hPanel = document.getElementById("h-panel");
-        hPanel.classList.toggle("reveal");
-    });
+    if (hPanelExit != null) {
+        hPanelExit.addEventListener("click", function(){
+            let panel = document.getElementById("test");
+            panel.classList.toggle("move");
+            let hPanel = document.getElementById("h-panel");
+            hPanel.classList.toggle("reveal");
+        });
+    }
+
 
     let gtShowBtn = document.getElementById("gt-show-btn");
-    gtShowBtn.addEventListener("click", function(e) {
-        e.preventDefault();
+    if(gtShowBtn != null){
+        gtShowBtn.addEventListener("click", function(e) {
+            e.preventDefault();
+    
+            let letters = document.getElementsByClassName("gt-anm");
+            for(let i = 0; i < letters.length; i++){
+                letters[i].classList.add("is-move");
+            }
+    
+            let pigs = document.getElementsByClassName("gt-pig");
+            for(let i = 0; i < pigs.length; i++){
+                pigs[i].classList.add("hellow");
+            }      
+        });
+    }
 
-        let letters = document.getElementsByClassName("gt-anm");
-        for(let i = 0; i < letters.length; i++){
-            letters[i].classList.add("is-move");
-        }
-
-        let pigs = document.getElementsByClassName("gt-pig");
-        for(let i = 0; i < pigs.length; i++){
-            pigs[i].classList.add("hellow");
-        }      
-    });
 });
