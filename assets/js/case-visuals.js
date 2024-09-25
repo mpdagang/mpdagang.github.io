97df16d57397249859afa4e11d2862bdcc39f5af
mpdagang95 <mariondagang@gmail.com>
2024-09-25 23:52:47 +0800

    Visuals: Add model interaction
    - control buttons
    - selector and keyframes instances for transform/animation
    - even listeners for button control and class toggle

--- assets/js/case-visuals.js
+++ assets/js/case-visuals.js
@@ -4,4 +4,11 @@ document.addEventListener("DOMContentLoaded", function() {
         let vSection = document.getElementById("section-01");
         vSection.classList.toggle("to-cube");
     });
+   const kyubiBtnList = document.getElementById("kyb-btn-wrapper");
+   kyubiBtnList.addEventListener("click", function (e) {
+        let toRemove = e.target.dataset.rmClass.split(',');
+        let kyubiContainer = document.getElementById("kyb-c-container");
+        kyubiContainer.classList.remove(...toRemove);
+        kyubiContainer.classList.toggle(e.target.dataset.adClass);
+   });
 });
